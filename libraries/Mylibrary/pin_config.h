9ac80af03695765254dfce5ab7929ad4e59693e8
LILYGO_L <135582120+Llgok@users.noreply.github.com>
2023-11-24 17:41:07 +0800

    Create Project

---
+++ libraries/Mylibrary/pin_config.h
@@ -0,0 +1,61 @@
+/*
+ * @Description: None
+ * @version: V1.0.0
+ * @Author: None
+ * @Date: 2023-08-16 14:24:03
+ * @LastEditors: LILYGO_L
+ * @LastEditTime: 2023-11-24 16:04:09
+ * @License: GPL 3.0
+ */
+#pragma once
+
+// MicroSD
+#define SD_CS 34
+#define SD_SCLK 36
+#define SD_MOSI 35
+#define SD_MISO 37
+
+// SPI
+#define SCLK 36
+#define MOSI 35
+
+// IIC
+#define IIC_SDA 17
+#define IIC_SCL 18
+
+// YDP395BT001-V2
+#define LCD_WIDTH 480
+#define LCD_HEIGHT 480
+#define LCD_CS 14
+#define LCD_DE 38
+#define LCD_VSYNC 40
+#define LCD_HSYNC 39
+#define LCD_PCLK 41
+#define LCD_B0 1
+#define LCD_B1 2
+#define LCD_B2 3
+#define LCD_B3 4
+#define LCD_B4 5
+#define LCD_G0 6
+#define LCD_G1 7
+#define LCD_G2 8
+#define LCD_G3 9
+#define LCD_G4 10
+#define LCD_G5 11
+#define LCD_R0 12
+#define LCD_R1 13
+#define LCD_R2 42
+#define LCD_R3 46
+#define LCD_R4 45
+#define LCD_BL 33
+
+
+// CST3240
+#define CST3240_Address 0x5A
+#define TOUCH_RST 4
+#define TOUCH_INT 21
+
+// KEY
+#define KEY1 48
+#define KEY2 47
+#define BOOT 0
